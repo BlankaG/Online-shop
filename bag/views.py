fa5bdf3e54f3d7aa73d727a7aca02b60edfe7549
BlankaG <blankacamb@hotmail.com>
2020-11-25 16:16:24 +0000

    Added zoom hover effect for cards

--- bag/views.py
+++ bag/views.py
@@ -7,10 +7,10 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -21,5 +21,6 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
-    
+  
